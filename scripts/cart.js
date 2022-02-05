1a6bbe1877e064b8967ec910f7dd947a3c9cd03e
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-02-05 18:02:37 +0000

    Rework File Struct and Script

---
+++ scripts/cart.js
@@ -0,0 +1,77 @@
+const Shoes = [
+    {
+        id: 1,
+        title: "Shoe1",
+        img: "img/shoe1.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nesciunt!",
+        price: 79.99
+    },
+    {
+        id: 2,
+        title: "Shoe2",
+        img: "img/shoe2.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nesciunt!",
+        price: 129.99
+    },
+    {
+        id: 3,
+        title: "Shoe3",
+        img: "img/shoe3.jpg",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nesciunt!",
+        price: 229.99
+    }
+]
+
+let Cart = [];
+
+//Access Modal 
+//Access ID of each Product
+let info = new bootstrap.Modal(document.getElementById("info-modal"));
+let cart = new bootstrap.Modal(document.getElementById("cart-modal"));
+let getAdd = document.querySelectorAll('.cart-btn')
+let product = document.querySelectorAll('.more-info');
+
+product.forEach(p => {
+    p.addEventListener("click", function (e) {
+        document.getElementById("modal-data").innerHTML = "<p>£" + Shoes[e.target.id].price;
+        info.show();
+    });
+});
+
+getAdd.forEach(p => {
+    var i = 0;
+    p.addEventListener("click", function (e) {
+        document.getElementById("modal-data").innerHTML = Shoes[e.target.id];
+        Cart += `<div id="increment-${i++} "class="row my-1">
+        <div class="col-2"><img class="w-100" src="${Shoes[e.target.id].img}"></div>
+        <div class="col-4">${Shoes[e.target.id].title}</div>
+        <div class="col-2"><input class="w-100" type="number" placeholder="1"></div>
+        <div class="col-2">${Shoes[e.target.id].price}</div>
+        <div class="col-2"><div id="btn-remove" class="btn btn-danger">X</div></div>
+        </div>`;
+        console.log(Cart);
+    })
+})
+
+
+function addToCart() {
+}
+
+
+
+document.getElementById("cart-btn").addEventListener("click", function () {
+
+    if (Cart.length === 0) {
+        document.getElementById("cart-body").innerHTML = "<strong>Your Cart is currently Empty</strong><p>Add to your Cart by pressing 'Add to Cart'";
+    } else {
+        document.getElementById("cart-body").innerHTML = Cart;
+        document.getElementById("btn-remove").addEventListener("click", function(e) { 
+            document.getElementById("modal-data").innerHTML = Shoes[e.target.id];
+            
+            
+
+        });
+    }
+
+    cart.show();
+});
