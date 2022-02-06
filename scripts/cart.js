672b81f30edc19703b18646ac2b8eef6ea84806f
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-02-06 16:49:13 +0000

    Check if Remove Button has been pressed

--- scripts/cart.js
+++ scripts/cart.js
@@ -23,6 +23,7 @@ const Shoes = [
 ]
 
 let Cart = [];
+let Count = [];
 
 //Access Modal 
 //Access ID of each Product
@@ -31,6 +32,7 @@ let cart = new bootstrap.Modal(document.getElementById("cart-modal"));
 let getAdd = document.querySelectorAll('.cart-btn')
 let product = document.querySelectorAll('.more-info');
 
+
 product.forEach(p => {
     p.addEventListener("click", function (e) {
         document.getElementById("modal-data").innerHTML = "<p>£" + Shoes[e.target.id].price;
@@ -38,40 +40,35 @@ product.forEach(p => {
     });
 });
 
+var i = 0;
+
 getAdd.forEach(p => {
-    var i = 0;
     p.addEventListener("click", function (e) {
-        document.getElementById("modal-data").innerHTML = Shoes[e.target.id];
-        Cart += `<div id="increment-${i++} "class="row my-1">
-        <div class="col-2"><img class="w-100" src="${Shoes[e.target.id].img}"></div>
-        <div class="col-4">${Shoes[e.target.id].title}</div>
-        <div class="col-2"><input class="w-100" type="number" placeholder="1"></div>
-        <div class="col-2">${Shoes[e.target.id].price}</div>
-        <div class="col-2"><div id="btn-remove" class="btn btn-danger">X</div></div>
+        Cart += `
+        <div id="shoe-row-${i++} "class="row my-1">
+            <div class="col-2"><img class="w-100" src="${Shoes[e.target.id].img}"></div>
+            <div class="col-4">${Shoes[e.target.id].title}</div>
+            <div class="col-2"><input class="w-100" type="number" placeholder="1"></div>
+            <div class="col-2">${Shoes[e.target.id].price}</div>
+            <div class="col-2"><div id="btn-remove" class="btn btn-danger">X</div></div>
         </div>`;
         console.log(Cart);
     })
 })
 
 
-function addToCart() {
-}
-
-
-
 document.getElementById("cart-btn").addEventListener("click", function () {
-
     if (Cart.length === 0) {
         document.getElementById("cart-body").innerHTML = "<strong>Your Cart is currently Empty</strong><p>Add to your Cart by pressing 'Add to Cart'";
     } else {
         document.getElementById("cart-body").innerHTML = Cart;
-        document.getElementById("btn-remove").addEventListener("click", function(e) { 
-            document.getElementById("modal-data").innerHTML = Shoes[e.target.id];
-            
-            
-
+        let remove = document.querySelectorAll(`#btn-remove`);
+        remove.forEach(element => {
+            element.addEventListener("click", function (e) {
+                console.log("Event Clicked! " + e.target.id);
+                // DO 
+            });
         });
     }
-
     cart.show();
 });
