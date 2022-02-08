c90a8b249e698c908d9cf20530a60295e2fd10bd
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-02-08 10:08:59 +0000

    Added Total Message

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,4 @@
+// Objects of our Shoe Products
 const Shoes = [
     {
         id: 1,
@@ -22,27 +23,35 @@ const Shoes = [
     }
 ]
 
+// Array to handle HTML
 let Cart = [];
+let Price = [];
+let total = 0;
+
+let i = 0;
 
 //Access Modal 
 //Access ID of each Product
 let info = new bootstrap.Modal(document.getElementById("info-modal"));
 let cart = new bootstrap.Modal(document.getElementById("cart-modal"));
-let getAdd = document.querySelectorAll('.cart-btn')
+
+// Obtain all classes with '. ', store in Memory
 let product = document.querySelectorAll('.more-info');
+let addToCart = document.querySelectorAll('.cart-btn')
 
-
+// Add EventListener of type 'click' to all buttons with '.more-info' class
 product.forEach(p => {
     p.addEventListener("click", function (e) {
         document.getElementById("modal-data").innerHTML = "<p>£" + Shoes[e.target.id].price;
         info.show();
-    });
+    })
 });
 
-var i = 0;
-
-getAdd.forEach(p => {
+// Add EventListener of type 'click' to all buttons with '.cart-info' class
+addToCart.forEach(p => {
     p.addEventListener("click", function (e) {
+        total = (total + Shoes[e.target.id].price);
+        console.log(total);
         Cart += `
         <div id="shoe-row-${i++} "class="row my-1">
             <div class="col-2"><img class="w-100" src="${Shoes[e.target.id].img}"></div>
@@ -50,24 +59,36 @@ getAdd.forEach(p => {
             <div class="col-2"><input class="w-100" type="number" placeholder="1"></div>
             <div class="col-2">${Shoes[e.target.id].price}</div>
             <div class="col-2"><div id="btn-remove" class="btn btn-danger">X</div></div>
-        </div>`;
-        console.log(Cart);
+        </div>
+        `;
+        Total = `
+        <div class="modal-footer mx-auto">
+            <div class="row">
+                <div class="col-6">
+                    <strong><p>Total: </strong>
+                </div>
+                <div class="col-6">
+                    <p>£${total.toFixed(2)}</p>
+                </div>
+            </div>
+        </div>
+        `
     })
-})
+});
 
 
 document.getElementById("cart-btn").addEventListener("click", function () {
     if (Cart.length === 0) {
         document.getElementById("cart-body").innerHTML = "<strong>Your Cart is currently Empty</strong><p>Add to your Cart by pressing 'Add to Cart'";
     } else {
-        document.getElementById("cart-body").innerHTML = Cart;
+        document.getElementById("cart-body").innerHTML = Cart + Total;
         let remove = document.querySelectorAll(`#btn-remove`);
         remove.forEach(element => {
             element.addEventListener("click", function (e) {
                 console.log("Event Clicked! " + e.target.id);
-                // DO 
-            });
-        });
-    }
+                document.getElementById("cart-body").innerHTML = "<p>Removed from the Cart</p>";
+            })
+        })
+    };
     cart.show();
 });
