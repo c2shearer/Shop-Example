9163c836de1f8eb48e7c55c84aea698421c981f4
Cameron Shearer <SHE16003894@btc.ac.uk>
2022-03-01 10:18:39 +0000

    Full Validation

--- scripts/user.js
+++ scripts/user.js
@@ -7,20 +7,26 @@ document.getElementById("submit").addEventListener("click", function () {
     const password = document.getElementById("password1").value;
     const confirmPassword = document.getElementById("password2").value;
 
-    // Validation
-    if (name === "" || phone === "" || email === "" || password === "") {
-        alert("An error with text-fields");
-    } else {
-        // Creating an Object within JavaScript with properties
-        var user = {
-            name: userName,
-            email: email,
-            password: password,
-            phone: phone
+    const checkBox = document.getElementById("checkBox");
+
+    if (!checkBox.checked) {
+        alert("Please 'tick' the box");
+    } else if (checkBox.checked) {
+        // Validation
+        if (name === "" || phone === "" || email === "" || password === "") {
+            alert("An error with text-fields");
+        } else if (password != confirmPassword) {
+            alert("Password doesn't match!");
+        } else {
+            // Creating an Object within JavaScript with properties
+            var user = {
+                name: name,
+                email: email,
+                password: password,
+                phone: phone
+            }
         }
     }
-
-    
 
     // Storing in the local storage of the Browser
     localStorage.setItem("userInfo", JSON.stringify(user));
